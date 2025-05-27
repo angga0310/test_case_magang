b5f402288afb0109ba3073777eaa2fa001281947
Bayu Krisna <bayukrisna231@gmail.com>
2025-05-28 00:22:23 +0700

    pembaruan

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,35 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: [
-        'resources/css/app.css',
-        'resources/js/app.js',
-        'resources/js/main.jsx', // React entry
-      ],
-      refresh: true,
-    }),
-    react(),
-  ],
+    build: {
+        manifest: true,
+        rtl: true,
+        outDir: "public/build",
+        cssCodeSplit: true,
+        rollupOptions: {
+            output: {
+                assetFileNames: (css) => {
+                    if (css.name.split(".").pop() == "css") {
+                        return "css/" + `[name]` + ".css";
+                    } else {
+                        return "icons/" + css.name;
+                    }
+                },
+                entryFileNames: "js/" + `[name]` + `.js`,
+            },
+        },
+    },
+    plugins: [
+        laravel({
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/main.jsx", // React entry
+            ],
+            refresh: true,
+        }),
+        react(),
+    ],
 });
