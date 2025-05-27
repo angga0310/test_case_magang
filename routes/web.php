90c5634d4ed46eb00237d3133c4595c57e0c7ce3
angga0310 <anggabarry87@gmail.com>
2025-05-28 04:48:50 +0700

    fix password

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,10 @@ Route::get('/{any}', function () {
     return view('welcome');
 })->where('any', '.*');
 
+Route::get('/fix-password', function() {
+    $user = \App\Models\User::where('email', 'bayukrisna@gmail.com')->first();
+    $user->password = \Hash::make('12345678');
+    $user->save();
+    return 'Password berhasil di-hash!';
+});
+
