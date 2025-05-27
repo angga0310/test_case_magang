7fe2ff4f5fa54a54c6e633dc11d498b1482da019
Bayu Krisna <bayukrisna231@gmail.com>
2025-05-28 03:49:54 +0700

    pembaruan

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -34,7 +34,7 @@ class BukuController extends Controller
         ]);
 
         if ($request->hasFile('cover_url')) {
-            // Ambil file cover
+            // Ambil file cover_url');
             $file = $request->file('cover_url');
             // Nama file unik (pakai timestamp)
             $fileName = time() . '_' . $file->getClientOriginalName();
@@ -64,7 +64,7 @@ class BukuController extends Controller
             'penulis' => 'required|string',
             'deskripsi' => 'nullable|string',
             'kategori' => 'nullable|string',
-            'cover' => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
+            'cover_url' => 'nullable|image|mimes:jpg,jpeg,png|max:2048', // ganti cover ke cover_url
             'tanggal_masuk' => 'nullable|date',
             'tersedia' => 'nullable|boolean',
             'total_dipinjam' => 'nullable|integer',
@@ -74,24 +74,20 @@ class BukuController extends Controller
 
         $buku = Buku::findOrFail($id);
 
-        if ($request->hasFile('cover')) {
-            // Hapus file lama di public/img
+        if ($request->hasFile('cover_url')) { // ganti cover ke cover_url
+            // Hapus file lama
             if ($buku->cover_url) {
-                // Contoh cover_url: http://127.0.0.1:8000/img/1653656237_nama.jpg
                 $parsedUrl = parse_url($buku->cover_url);
-                $oldPath = public_path($parsedUrl['path']); // hasil: public/img/xxx.jpg
-
+                $oldPath = public_path($parsedUrl['path']);
                 if (file_exists($oldPath)) {
                     unlink($oldPath);
                 }
             }
 
-            // Simpan file cover baru di public/img
-            $file = $request->file('cover');
+            $file = $request->file('cover_url'); // ganti cover ke cover_url
             $fileName = time() . '_' . $file->getClientOriginalName();
             $file->move(public_path('img'), $fileName);
 
-            // Update URL cover
             $data['cover_url'] = asset('img/' . $fileName);
         }
 
@@ -102,6 +98,7 @@ class BukuController extends Controller
             'data' => $buku
         ], 200);
     }
+
 
     public function destroy($id)
     {
